9ffa33e62182dde4b57cba6ecd5e0f3f9fbb16cb
Thenujan-Dev <thenujanc@gmail.com>
2025-05-22 07:45:18 +0530

    feat:creating blog delete and blog update route

---
+++ app/api/blog/[id]/route.ts
@@ -0,0 +1,34 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { handleError } from "../../helpers/handleError";
+import { BlogSchema } from "@/schemas/blog.schema";
+
+export const DELETE = async (req: NextRequest) => {
+  try {
+    const idUrl = req.url;
+    const userId = idUrl.split("blog/")[1];
+    await prisma.blog.delete({ where: { id: userId } });
+    return NextResponse.json({
+      success: true,
+      message: "User Deleted Succesfully!",
+    });
+  } catch (error) {
+    return handleError({ error, defaultError: "Failed delete blog" });
+  }
+};
+
+export const PUT = async (req: NextRequest) => {
+  try {
+    const idUrl = req.url;
+    const body = await req.json();
+    const validatedData = BlogSchema.parse(body);
+    const userId = idUrl.split("blog/")[1];
+    await prisma.blog.update({ where: { id: userId }, data: validatedData });
+    return NextResponse.json({
+      success: true,
+      message: "User Updated Succesfully!",
+    });
+  } catch (error) {
+    return handleError({ error, defaultError: "Failed Updated blog" });
+  }
+};
--- app/generated/prisma/index.js
+++ app/generated/prisma/index.js
@@ -133,7 +133,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\PC\\Desktop\\Projects\\blog app\\blog\\app\\generated\\prisma",
+      "value": "C:\\Users\\PC\\Desktop\\Projects\\blog\\app\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -147,7 +147,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\PC\\Desktop\\Projects\\blog app\\blog\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Users\\PC\\Desktop\\Projects\\blog\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
